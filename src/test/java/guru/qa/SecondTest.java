a9bd23401efa6f4e95f68088bd7428b9c7836f62
Pivovarov Denis <denis.pivovarov@simbirsoft.com>
2025-01-24 03:05:44 +0400

    Изменил false на true в 9 строчке

--- src/test/java/guru/qa/SecondTest.java
+++ src/test/java/guru/qa/SecondTest.java
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test;
 public class SecondTest {
     @Test
     void secondTest() {
-        Assertions.assertTrue(false);
+        Assertions.assertTrue(true);
     }
 }
